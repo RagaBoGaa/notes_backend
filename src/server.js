e6997a17c5427de7b748b2ac63924f1d4c3ead38
Ragab <ragab.magdy59@gmail.com>
2025-07-10 23:51:40 +0300

    Update verce.json to simplify build configuration and modify server.js to allow CORS from multiple origins

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use(rateLimiter);
 app.use(multer().none());
 app.use(
   cors({
-    // origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://notesfs.vercel.app'],
     credentials: true,
   })
 );
